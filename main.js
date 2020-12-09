5bdf38bffa97c1ab5148375e6991227ddcc8baa5
Pavel <Golovin.Pavel@outlook.com>
2020-12-09 21:56:29 +0300

    Добавляем карточки репозитория в список ниже, очищаем поле ввода и автокомплит

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const field = document.querySelector('.search__input');
-const dataList = document.querySelector('.search__autocomplete');
+const dataList = document.querySelector('.search__repo-list');
+const reposList = document.querySelector('.repo-container__list');
 let lastTimeout;
 
 const getRepositories = async (value) => {
@@ -12,14 +13,35 @@ const getRepositories = async (value) => {
   }
 }
 
+const newOptionClickHandler = (evt) => {
+  const target = evt.target
+  let newCard = document.createElement('li');
+  newCard.classList.add('repo-container__item');
+  let name = document.createElement('p');
+  name.textContent = `Name: ${target.textContent}`;
+  let owner = document.createElement('p');
+  owner.textContent = `Owner: ${target.dataset.owner}`;
+  let stars = document.createElement('p');
+  stars.textContent = `Stars: ${target.dataset.stars}`;
+  newCard.append(name);
+  newCard.append(owner);
+  newCard.append(stars);
+  reposList.append(newCard);
+  field.value = "";
+  while (dataList.firstChild) {
+    dataList.removeChild(dataList.firstChild);
+  }
+}
+
 const renderAutoComplete = (repo) => {
   const [repoOwner, repoName, repoStars] = [...repo["full_name"].split("/"), repo["watchers"]];
-  const newOption = document.createElement("option");
-  newOption.value = repoName;
+  const newOption = document.createElement("li");
+  newOption.classList.add("repo-list__item");
+  newOption.innerHTML = repoName;
   newOption.dataset.owner = repoOwner;
   newOption.dataset.stars = repoStars;
   dataList.append(newOption);
-  newOption.onclick()
+  newOption.addEventListener("click", newOptionClickHandler);
 }
 
 const fieldInputHandler = (evt) => {
@@ -29,7 +51,9 @@ const fieldInputHandler = (evt) => {
   lastTimeout = setTimeout(async () => {
     const target = evt.target;
     const result = await getRepositories(target.value);
-    dataList.innerHTML = "";
+    while (dataList.firstChild) {
+      dataList.removeChild(dataList.firstChild);
+    }
     result.forEach((repo) => renderAutoComplete(repo));
   }, 300)
 }
