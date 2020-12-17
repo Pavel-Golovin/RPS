6fb82da69730211967ef485a05380887c4174c3e
Pavel <Golovin.Pavel@outlook.com>
2020-12-18 00:12:13 +0300

    Вынес загрузку в отдельный модуль + рендерим карточку теперь по темплейту

---
+++ js/load.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(() => {
+  window.loadRepositories = async (value) => {
+    try {
+      const response = await fetch(`https://api.github.com/search/repositories?q=${value}`)
+      const data = await response.json()
+      return data.items.slice(0, 5);
+    } catch (e) {
+      return [];
+    }
+  }
+})();
